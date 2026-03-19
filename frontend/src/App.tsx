ac0a0f65920cafd03a0f10fb8276839ac7b087c0
sachingupta4545 <sachin.gupta6292@gmail.com>
2026-03-19 22:21:26 +0530

    adding redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { store } from './redux/Store'
+import { Provider } from 'react-redux'
 
 const queryClient = new QueryClient();
 
 function App() {
   return <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </QueryClientProvider>;
 }
 
