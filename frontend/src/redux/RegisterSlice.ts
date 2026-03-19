164dd5951b22bd0ddca632f978eefde2743f300e
sachingupta4545 <sachin.gupta6292@gmail.com>
2026-03-19 22:49:59 +0530

    add slicer logic

---
+++ frontend/src/redux/RegisterSlice.ts
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice, isRejectedWithValue } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { Register } from '@/services/authService'
+
+export interface RegisterState {
+    items: [],
+    loading: boolean,
+    error: any,
+}
+
+const RegisterAuth = createAsyncThunk('register', Register)
+const initialState: RegisterState = {
+    items: [],
+    loading: false,
+    error: null,
+}
+
+export const RegisterSlice = createSlice({
+    name: 'RegisterSlice',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(RegisterAuth.fulfilled, (state, action) => {
+            state.items = action.payload
+            state.loading = false
+            state.error = null
+        })
+        builder.addCase(RegisterAuth.rejected, (state, action) => {
+            state.error = action.payload
+            state.loading = false
+        })
+        builder.addCase(RegisterAuth.pending, (state) => {
+            state.loading = true
+        })
+    }
+})
+
+// Action creators are generated for each case reducer function
+// export const { } = RegisterSlice.actions
+
+export default RegisterSlice.reducer
--- frontend/src/redux/Store.ts
+++ frontend/src/redux/Store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import RegisterReducer from './RegisterSlice'
 
 export const store = configureStore({
-  reducer: {},
+    reducer: {
+        RegisterReducer,
+    },
 })
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
