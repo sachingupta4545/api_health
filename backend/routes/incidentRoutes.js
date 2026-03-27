6b42708a5878ec8f5de5354efaa671a85aa8b473
sachingupta4545 <sachin.gupta6292@gmail.com>
2026-03-27 07:54:00 +0530

    path correction

--- backend/routes/incidentRoutes.js
+++ backend/routes/incidentRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect } from "../middlewares/authMiddleware.js";
 import {
     getIncidents,
     getIncidentById,
